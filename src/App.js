f9f25305a6e60958c3d156d26dccc46da12f3b3a
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-05-23 13:04:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [posts, setPost] = useState([{ id: 1, title:'First post', date:new Date()}])
+  const [value, setValue] = useState('');
+  const [errorAlert, setErrorAlert] = useState('');
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+
+  const addValue = () => {
+    setPost(
+      [
+        ...posts,
+        {id: posts.length+1, title: value, date: new Date()}
+      ]
+    );
+    setValue('');
+    setErrorAlert('');
+  }
+
+  const handleClick = () => {
+    value ? addValue() : setErrorAlert('Post vazio.')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Post</th>
+          <th>Data</th>
+        </tr>
+        {posts.map(post => <Post key={post.id} id={post.id} title={post.title} date={post.date} />)}
+      </table>
+      <div>
+      <input
+        type='text'
+        value={value}
+        onChange={handleChange}
+        required
+      />
+      <button onClick={handleClick}>Click</button>
+      </div>
+      <span>{errorAlert}</span>
+      </div>
     </div>
   );
+}
+
+function Post(props) {
+  return (
+    <tr>
+      <td>{props.id}</td>
+      <td>{props.title}</td>
+      <td>{props.date.toLocaleString('pt-br')}</td>
+    </tr>
+  )
 }
 
 export default App;
